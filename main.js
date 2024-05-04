87d4963f1a3be31954c8e88611c619ea3840ecc1
SardarNazeer <sardarnazeer1108@gmail.com>
2024-05-05 01:24:04 +0500

    2 practice question

--- main.js
+++ main.js
@@ -30,3 +30,37 @@ class toyataCar {
 
 let audi = new toyataCar("audi"); // constructor
 let corola = new toyataCar(); // constructor undefined
+
+
+// q1) you are creating a website for your college. Creating a class user with 2 properties, name & email. it also has a method called viewData() that allow user to view website data 
+
+let data = "secret data information";
+
+class user{
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData(){
+        console.log(data);
+    }
+}
+
+let std1 = new user("sardar nazeer","sardar@gmail.com");
+let std2 = new user("ali sher","ali@gmail.com");
+
+let teacher1 = new user("dean","dean@gmail.com");
+
+// q2) creating a new class admin which inherit from user. Add a new method called editData to Admin that allow it to edit website data
+
+class admin extends user{
+    constructor(name,email){
+        super(name,email);
+    }
+    editData(){
+        data = "new value";
+    }
+}
+
+let admin = new admin("admin","admin@gmail.com");
